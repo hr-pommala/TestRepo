ec1d2c4bdfeea47dcb30c57d4556b1ebe61f4860
sandeepkumare <Sandeepkumar.E@pommala.com>
2025-08-22 10:20:48 +0530

    updated maven folder structure.

---
+++ src/main/java/com/sphuta/main/SphutaMainApplication.java
@@ -0,0 +1,13 @@
+package com.sphuta.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SphutaMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SphutaMainApplication.class, args);
+    }
+
+}
